7ea8e09761752e9a6b7902907782ce8d20f699a5
Sam LaManna <slamanna212@gmail.com>
2025-06-03 21:23:05 -0400

    revert event type i didnt test correctly

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ def github_webhook():
         return jsonify({"message": "Not an issue event"}), 200
         
     # Only process when issues are typed
-    if data['action'] != 'opened':
+    if data['action'] != 'typed':
         logger.info(f"Skipping issue event with action: {data['action']}")
         return jsonify({"message": "Not an issue typing event"}), 200
     
